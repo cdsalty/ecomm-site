479b84ddd301873c8a45ec5e589ddd1faa3ac459
cdsalty <soltiscd@gmail.com>
2019-08-06 23:35:31 -0400

    creating directory componment to map images

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.style.scss';
+
+// CREATING OUR MENU-ITEM COMPONENT 
+const MenuItem = ({ title }) => (
+  <div className = 'menu-item'>
+    <div className = 'content'>
+      <h1 className = 'title'>{title}</h1>
+      <span className = 'subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
+/*
+Destructuring: by using ({title}), we're simply taking title from props; same as props.title
+*/
