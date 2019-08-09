fb4545a819b0923abdca6a9a92e13dd95a36b682
cdsalty <soltiscd@gmail.com>
2019-08-08 23:08:20 -0400

    more config

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,12 +6,15 @@ import './menu-item.styles.scss';
 // CREATING OUR MENU-ITEM COMPONENT 
   // - div style has prop values equal to css
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div style = {{
-    backgroundImage: `url(${imageUrl})`
-  }}
-  className = {`${size} menu-item`}  >
+  <div className = {`${size} menu-item`}>
+    <div
+      className = 'background-image'
+      style = {{
+        backgroundImage: `url(${imageUrl})`
+      }}
+  />
     <div className = 'content'>
-      <h1 className = 'title'>{title}</h1>
+      <h1 className = 'title'>{title.toUpperCase()}</h1>
       <span className = 'subtitle'>SHOP NOW</span>
     </div>
   </div>
@@ -20,5 +23,8 @@ const MenuItem = ({ title, imageUrl, size }) => (
 export default MenuItem;
 /*
 Destructuring: by using ({title}), we're simply taking title from props; same as 'props.title'
+
+Learned how to uppercase the title. Since javascript, any methods can be used as well.
+
 */
 
