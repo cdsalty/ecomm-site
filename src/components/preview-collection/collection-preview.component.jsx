641740c0447bfb45a9eb15736aa182a2270c434d
cdsalty <soltiscd@gmail.com>
2019-08-20 22:42:10 -0400

    have to stop; in middle of CollectionPreview

---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import './collection-preview.styles.scss';
+
+// functional component that returns an outer div that contains the entire collection. Inside of it will 
+// be a tile as well as the actual items themselves. Those items will be held in a div and this will be 
+// actual preview where this will be overall collection preview
+
+const CollectionPreview = () => {
+  <div>
+    <h1>Title</h1>
+    <div className = 'preview'></div>
+  </div>
+}
